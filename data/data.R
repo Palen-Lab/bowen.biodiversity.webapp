350c5fb88fe069e37b694b3a9edaa9109d3ddc9c
Jay Matsushiba <jay.matsushiba@gmail.com>
2025-04-10 13:50:40 -0700

    data/data.R documentation

---
+++ data/data.R
@@ -0,0 +1,5 @@
+#' Fish Bearing Streams Spatial Data
+#'
+#' Spatial data, originally provided as shapefile, of fish bearing streams on
+#' Bowen Island. This layer was provided by Alan Whitehead on April 9th 2025.
+"bowen_fish_whitehead_consultants"
