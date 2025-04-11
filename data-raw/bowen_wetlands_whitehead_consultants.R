a7babba13703713d985754e5c798fb6a4d6a3ee9
Jay Matsushiba <jay.matsushiba@gmail.com>
2025-04-11 11:18:15 -0700

    wetland and ponds layers from Alan Whitehead

--- data-raw/bowen_wetlands_whitehead_consultants.R
+++ data-raw/bowen_wetlands_whitehead_consultants.R
@@ -3,8 +3,8 @@ library(sf)
 library(here)
 library(dplyr)
 
-bowen_ponds_whitehead_consultants <- st_read(here("data-raw/bowen_wetlands_whitehead_consultants/ponds-aw.shp"))
+bowen_ponds_whitehead_consultants <- st_read(here("data-raw/bowen_wetlands_whitehead_consultants/ponds-aw.gpkg"))
 usethis::use_data(bowen_ponds_whitehead_consultants, overwrite = TRUE)
 
-bowen_wetlands_whitehead_consultants <- st_read(here("data-raw/bowen_wetlands_whitehead_consultants/wetlands2015.shp"))
+bowen_wetlands_whitehead_consultants <- st_read(here("data-raw/bowen_wetlands_whitehead_consultants/wetlands2015.gpkg"))
 usethis::use_data(bowen_wetlands_whitehead_consultants, overwrite = TRUE)
--- data/data.R
+++ data/data.R
@@ -2,4 +2,19 @@
 #'
 #' Spatial data, originally provided as shapefile, of fish bearing streams on
 #' Bowen Island. This layer was provided by Alan Whitehead on April 9th 2025.
+#'
+#' @format ## `bowen_fish_whitehaed_consultants`
+#' A data frame with 367 rows and 10 columns:
+#' \describe{
+#'   \item{LENGTH}{Length}
+#'   \item{SOURCE}{Source of the information}
+#'   \item{ACCURACY}{Accuracy}
+#'   \item{CREEK_NAME}{Name of the creek}
+#'   \item{COMMENT}{Comment}
+#'   \item{FISH_PRES}{Presence of fish}
+#'   \item{TRIB_TO_FI}{Tributary to fish}
+#'   \item{OCP_CLASS}{OCP class, fish bearing status}
+#'   \item{Notes}{Additional notes}
+#'   \item{geometry}{sf geometry}
+#' }
 "bowen_fish_whitehead_consultants"
