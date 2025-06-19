4529ded96cc4e333d2fe9859304559f661ef5a37
Jay Matsushiba <jay.matsushiba@gmail.com>
2025-06-19 12:45:44 +0900

    created relative biodiversity by potential development units map

--- R/plots.R
+++ R/plots.R
@@ -222,3 +222,137 @@ bowen_map_toppct <- function(raster_layer,
 
 }
 
+
+#' Bowen Map Plotting (Improved)
+#'
+#' @description
+#' This function takes aas an input a function that returns ggplot components
+#' (See: https://ggplot2-book.org/programming.html). This allows the
+#' bowen_map_ggplot function to be able to add ggplot components before and
+#' after the input layers. This is useful as this allows additional map
+#' elements, such as road / trails, to be added above the main feature layers.
+#'
+#' @param gg_func function, that returns ggplot components
+#' @param title Text, title for map
+#' @param subtitle Text, subtitle for map
+#' @param caption Text, caption for map
+#' @param pal Palette, from colorspace package
+#' @returns ggplot
+#'
+#' @import ggplot2
+#' @export
+#'
+bowen_map_ggplot <- function(gg_func,
+                             title = "Default",
+                             subtitle = "Default",
+                             caption = "Default",
+                             pal = "ag_GrnYl") {
+  #### CHECK INPUT PARAMETERS ####
+  # Check if gg_func can be added to ggplot()
+  if(class(gg_func) == "function") {
+    ggplot() + gg_func() # Error and exit if this is not possible
+  } else {
+    stop("gg_func needs to be a function that returns ggplot components. See https://ggplot2-book.org/programming.html. Did you add parentheses like this gg_func() by accident?")
+  }
+
+  #### ADDITIONAL MAP ELEMENTS ####
+  # Map element colours
+  trails_colour <- "brown"
+  roads_colour <- "darkgrey"
+  # Bowen Island mask raster
+  bowen_mask <- terra::rast(here("inst/extdata/bowen_mask.tif")) %>%
+    project("EPSG: 3857")
+  bowen_mask_ext <- bowen_mask %>%
+    ext()
+  bowen_mask_sf <- as.polygons(bowen_mask, extent=T) %>%
+    st_as_sf() %>%
+    st_transform(3857) %>%
+    st_buffer(1000)
+  # Shoreline
+  bowen_shoreline <- st_read(here("data-raw/bowen_island_shoreline_w_hutt.gpkg"), layer = "bowen_island_shoreline_w_hutt__bowen_islands") %>%
+    st_transform(3857)
+  # Create ocean polygon, need this to reduce appearance of jagged edges from raster along coastline
+  bowen_ocean_sf <- st_difference(st_union(bowen_mask_sf), st_union(bowen_shoreline))
+  bowen_ocean_linewidth <- 0.5
+  bowen_ocean_colour <- "#dbdbdc"
+  # Create basemap
+  basemap_for_plot <- basemaps::basemap_terra(ext = bowen_mask,
+                                              map_service = "maptiler",
+                                              map_type = "backdrop",
+                                              map_token = "baL4WLstSFqSHP2fnYrE")
+
+  #### OUTPUT GGPLOT ####
+  output_plot <- ggplot2::ggplot() +
+    # Ensure consistent scale for all maps made with this function
+    ggplot2::scale_x_continuous(expand = c(0,0), limits = c(bowen_mask_ext[1], bowen_mask_ext[2])) +
+    ggplot2::scale_y_continuous(expand = c(0,0), limits = c(bowen_mask_ext[3], bowen_mask_ext[4])) +
+    # Input parameters to determine labels in ggplot map
+    ggplot2::labs(
+      title = title,
+      subtitle = subtitle,
+      caption = caption
+    ) +
+    # Theme
+    ggplot2::theme_bw() +
+    ggplot2::theme(
+      plot.title = ggtext::element_textbox_simple(
+        size = 20,
+        margin = ggplot2::margin(0, 0, 20, 0)
+      ),
+      plot.subtitle = ggtext::element_textbox_simple(
+        lineheight = 1.5,
+        margin= ggplot2::margin(0, 0, 10, 0)
+      ),
+      axis.text.y = ggplot2::element_text(
+        angle = 90, vjust = 1, hjust = 0.5
+      ),
+      legend.position = "bottom",
+      legend.title = ggplot2::element_text(size = 10),
+      legend.key.height = ggplot2::unit(0.5, "cm"),
+      legend.key.width = ggplot2::unit(1.5, "cm"),
+      legend.direction = "horizontal",
+      legend.box = "horizontal",
+      legend.box.margin = margin(1, 1, 1, 1),
+      plot.margin = unit(c(1.3,0.3,0.8,0), "cm")
+    ) +
+    # Add basemap
+    tidyterra::geom_spatraster_rgb(data = basemap_for_plot) +
+    # Reset fill / colour, need this to have multiple fill / colours for each layer
+    ggnewscale::new_scale_fill() +
+    ggnewscale::new_scale_colour() +
+    # Add input ggplot components
+    gg_func() +
+    # Add additional map elements
+    geom_sf(data = bowen_ocean_sf,
+            linewidth = bowen_ocean_linewidth,
+            fill = bowen_ocean_colour
+    ) +
+    ggplot2::geom_sf(data = bowen_trails,
+                     linewidth = 0.3,
+                     aes(color = trails_colour)
+    ) +
+    ggplot2::geom_sf(data = bowen_roads,
+                     linewidth = 0.5,
+                     aes(color = roads_colour)
+    ) +
+    scale_colour_identity(name = "",
+                          breaks = c(trails_colour, roads_colour),
+                          labels = c("Trails", "Roads"),
+                          guide = "legend"
+    ) +
+    ggspatial::annotation_scale(
+      location = "br",
+      bar_cols = c("grey60", "white")
+    ) +
+    ggspatial::annotation_north_arrow(
+      location = "br", which_north = "true",
+      pad_x = unit(0.2, "in"), pad_y = unit(0.4, "in"),
+      style = ggspatial::north_arrow_nautical(
+        fill = c("grey40", "white"),
+        line_col = "grey20"
+      )
+    )
+  #### RETURN OUTPUT ####
+  output_plot
+}
+
