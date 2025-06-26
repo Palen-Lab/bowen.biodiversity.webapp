6d298bd47ec621c16ecbe203a0710c1cb21dec57
Jay Matsushiba <jay.matsushiba@gmail.com>
2025-06-26 10:20:50 +0900

    add remove_by_quantile function

--- R/utils-sdm.R
+++ R/utils-sdm.R
@@ -134,3 +134,37 @@ bowen_rasterize <- function(sf) {
                      touches = T,
                      background = NA) # Rasterize to match mask, set background values to NA
 }
+
+#' Only keep top or bottom cell values by quantile
+#'
+#' @param x SpatRaster
+#' @param prob Quantile between 0 and 1
+#' @param direction "top" or "bottom", for direction of keep / discard cell values
+#'
+#' @returns SpatRaster
+#' @export
+remove_by_quantile <- function(x, prob, direction = "top") {
+  val <- x %>%
+    values() %>%
+    quantile(probs = prob, na.rm = T)
+  min_val <- x %>%
+    values() %>%
+    min(na.rm = T)
+  max_val <- x %>%
+    values() %>%
+    max(na.rm = T)
+  if(direction == "top") {
+    m <- matrix(c(
+      min_val, val, NA,
+      val, max_val, 1
+    ), ncol = 3, byrow = T)
+  } else if (direction == "bottom") {
+    m <- matrix(c(
+      min_val, val, 1,
+      val, max_val, NA
+    ), ncol = 3, byrow = T)
+  }
+  mask_quantile <- x %>%
+    classify(m,, include.lowest = T)
+  mask_quantile * x
+}
