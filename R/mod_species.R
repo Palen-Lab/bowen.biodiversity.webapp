5898428e3e5524f46adfb174de48eba906fcefaf
Jay Matsushiba <jay.matsushiba@gmail.com>
2025-06-26 17:30:25 +0900

    adding species richness function to module

--- R/mod_species.R
+++ R/mod_species.R
@@ -8,10 +8,20 @@
 #'
 #' @importFrom shiny NS tagList
 mod_species_ui <- function(id) {
-  ns <- NS(id)
   tabPanel(
     "species_panel",
-    h1("Species")
+    selectInput(
+      NS(id, "selectSpeciesGroup"),
+      "Select Species Group:",
+      choices = c("All Species" = "all",
+                  "Threatened Species" = "threatened",
+                  "Bird Species" = "birds",
+                  "Small Mammals" = "sm_mammals",
+                  "Amphibians / Reptile" = "herptiles",
+                  "Summed Species Distribution Models" = "sum_sdms"),
+      selected = "all"
+    ),
+    htmlOutput(NS(id, "sidebarInfo"))
   )
 }
 
@@ -22,28 +32,113 @@ mod_species_server <- function(id, map_id, parent_session){
   moduleServer(id, function(input, output, session){
     ns <- session$ns
 
-    species_richness <- terra::rast(here::here("inst/extdata/bowen_sdm_richness.tif")) %>%
-      terra::project("epsg:4326")
-    species_richness_group <- "Species Richness"
-    species_richness_domain <- c(0, terra::minmax(species_richness)[2])
-    species_richness_pal <- leaflet::colorNumeric(c('#edf8fb','#b2e2e2','#66c2a4','#2ca25f','#006d2c'), species_richness_domain,
-                                                 na.color = "transparent")
+    #### Update raster when input changes ####
+    select_raster <- reactiveVal({
+      terra::rast(here::here("inst/extdata/bowen_sdm_richness.tif")) %>%
+        terra::project("epsg:4326")
+    })
+    observeEvent(input$selectSpeciesGroup, {
+      # Options for species richness
+      # 1. All Species Richness
+      # 2. Threatened Species Richness
+      # 3. Bird Species Richness
+      # 4. Small Mammal Species Richness
+      # 5. Amphibian / Reptile Species Richness
+      # 6. Summed Species Distribution Models
 
-    leaflet::leafletProxy(mapId = map_id,
-                          session = parent_session) %>%
-      leaflet::clearControls() %>%
-      leaflet::clearImages() %>%
-      leaflet::addRasterImage(
-        x = species_richness,
-        layerId = "species_richness_raster",
-        colors = species_richness_pal
-      ) %>%
-      leaflet::addLegend(
-        pal = species_richness_pal,
-        layerId = "species_richness_legend",
-        values =  species_richness_domain,
-        title = "Species Richness"
-      )
+      if(input$selectSpeciesGroup == "all") {
+        terra::rast(here::here("inst/extdata/bowen_sdm_richness.tif")) %>%
+          terra::project("epsg:4326") %>%
+          select_raster()
+      } else if (input$selectSpeciesGroup == "threatened") {
+        # TODO: replace with actual
+        terra::rast(here::here("inst/extdata/2_species/threatened_richness.tif")) %>%
+          terra::project("epsg:4326") %>%
+          select_raster()
+      } else if (input$selectSpeciesGroup == "birds") {
+        # TODO: replace with actual
+        terra::rast(here::here("inst/extdata/2_species/birds_richness.tif")) %>%
+          terra::project("epsg:4326") %>%
+          select_raster()
+      } else if (input$selectSpeciesGroup == "sm_mammals") {
+        # TODO: replace with actual
+        terra::rast(here::here("inst/extdata/2_species/sm_mammals_richness.tif")) %>%
+          terra::project("epsg:4326") %>%
+          select_raster()
+      } else if (input$selectSpeciesGroup == "herptiles") {
+        # TODO: replace with actual
+        terra::rast(here::here("inst/extdata/2_species/herptiles_richness.tif")) %>%
+          terra::project("epsg:4326") %>%
+          select_raster()
+      } else if (input$selectSpeciesGroup == "sum_sdms") {
+        # TODO: replace with actual
+        terra::rast(here::here("inst/extdata/2_species/sum_sdms.tif")) %>%
+          terra::project("epsg:4326") %>%
+          select_raster()
+      }
+
+      species_richness_group <- "Species Richness"
+      species_richness_domain <- c(0, terra::minmax(select_raster())[2])
+      species_richness_pal <- leaflet::colorNumeric(c('#edf8fb','#b2e2e2','#66c2a4','#2ca25f','#006d2c'), species_richness_domain,
+                                                    na.color = "transparent")
+
+      # Update map
+      leaflet::leafletProxy(mapId = map_id,
+                            session = parent_session) %>%
+        leaflet::clearControls() %>%
+        leaflet::clearImages() %>%
+        leaflet::addRasterImage(
+          x = select_raster(),
+          layerId = "species_richness_raster",
+          colors = species_richness_pal
+        ) %>%
+        leaflet::addLegend(
+          pal = species_richness_pal,
+          layerId = "species_richness_legend",
+          values =  species_richness_domain,
+          title = "Species Richness"
+        )
+    })
+
+    #### Update sidebar content based on selection ####
+
+
+    observeEvent(input$selectSpeciesGroup, {
+      # Options for species richness
+      # 1. All Species Richness
+      # 2. Threatened Species Richness
+      # 3. Bird Species Richness
+      # 4. Small Mammal Species Richness
+      # 5. Amphibian / Reptile Species Richness
+      # 6. Summed Species Distribution Models
+
+      if(input$selectSpeciesGroup == "all") {
+        output$sidebarInfo <- renderUI({
+          h1("All Species")
+        })
+      } else if (input$selectSpeciesGroup == "threatened") {
+        output$sidebarInfo <- renderUI({
+          h1("Threatened Species")
+        })
+      } else if (input$selectSpeciesGroup == "birds") {
+        output$sidebarInfo <- renderUI({
+          h1("Bird Species")
+        })
+      } else if (input$selectSpeciesGroup == "sm_mammals") {
+        output$sidebarInfo <- renderUI({
+          h1("Small Mammal Species")
+        })
+      } else if (input$selectSpeciesGroup == "herptiles") {
+        output$sidebarInfo <- renderUI({
+          h1("Amphibian and Reptile Species")
+        })
+      } else if (input$selectSpeciesGroup == "sum_sdms") {
+        output$sidebarInfo <- renderUI({
+          h1("Summed Species Distribution Models")
+        })
+      }
+    })
+
   })
 }
 
