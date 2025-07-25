bab19eb37b5acb277aa10e42ea9706187775afb1
Jay Matsushiba <jay.matsushiba@gmail.com>
2025-07-25 16:57:10 +0900

    wildfire threats done for now

--- R/mod_threats.R
+++ R/mod_threats.R
@@ -11,8 +11,21 @@ mod_threats_ui <- function(id) {
   ns <- NS(id)
   tabPanel(
     "threats_panel",
-    h1("Threats"),
-    p("Under Construction")
+    selectInput(
+      NS(id, "selectGroup"),
+      "Select Threat:",
+      choices = c(
+        "Development" = "development",
+        "Wildfire" = "wildfire"
+      ),
+      selected = "development"
+    ),
+    bslib::card(
+      bslib::card_body(
+        htmlOutput(NS(id, "sidebarInfo")),
+      )
+    ),
+    docs_link
   )
 }
 
@@ -22,20 +35,83 @@ mod_threats_ui <- function(id) {
 mod_threats_server <- function(id, map_id, parent_session){
   moduleServer(id, function(input, output, session){
     ns <- session$ns
+    #### Update raster when input changes ####
+    select_raster <- reactiveVal({
+      terra::rast(here::here("inst/extdata/3_habitats/total_habitat_richness.tif")) %>%
+        terra::project("epsg:4326")
+    })
+    observeEvent(input$selectGroup, {
+      #### DEVELOPMENT ####
+      if(input$selectGroup == "development") {
+        # Update Leaflet Map Parameters
+        raster_group <- "Development Potential"
+        terra::rast(here::here("inst/extdata/3_habitats/total_habitat_richness.tif")) %>%
+          terra::project("epsg:3857", method = "near") %>%
+          select_raster()
+        raster_domain <- terra::values(select_raster()) %>%
+          unique() %>%
+          sort()
+        raster_labels <- raster_domain
+        raster_pal <- leaflet::colorNumeric(
+          c('#edf8fb','#b2e2e2','#66c2a4','#2ca25f','#006d2c'),
+          raster_domain,
+          na.color = "transparent"
+        )
+        # Update Specfic Sidebar
+        output$sidebarInfo <- renderUI({
+          tagList(
+            h1("Development"),
+            util_ui_simple_legend(low_colour = '#edf8fb', high_colour = '#006d2c', low_label =  "Fewer Habitat Types", high_label =  "More Habitat Types"),
+            p("Bowen Island features a rich mosaic of habitats—ranging from mature temperate rainforests and dry coastal bluffs to freshwater wetlands, lakes, streams, and intertidal shores—each supporting distinct communities of plants, animals, and fungi. This diversity of ecosystems underpins high ecological resilience and biodiversity, making the island a vital refuge for both terrestrial and aquatic life.")
+          )
+        })
+      }
+      #### WILDFIRE ####
+      else if (input$selectGroup == "wildfire") {
+        # Update Leaflet Map Parameters
+        raster_group <- "Relative Wildfire Vuln."
+        fire_index_simple <- terra::rast(here::here("inst/extdata/6_threats/fire_index_32m.tif")) %>%
+          terra::project("epsg:3857", method = "near")
+        NAflag(fire_index_simple) <- 4294967296
+        select_raster(fire_index_simple)
 
-    # species_richness <- terra::rast(here::here("inst/extdata/bowen_sdm_richness.tif")) %>%
-    #   terra::project("epsg:4326")
-    # species_richness_group <- "Species Richness"
-    # species_richness_domain <- c(0, terra::minmax(species_richness)[2])
-    # species_richness_pal <- leaflet::colorNumeric(c('#edf8fb','#b2e2e2','#66c2a4','#2ca25f','#006d2c'), species_richness_domain,
-    #                                               na.color = "transparent")
-    #
-    # leaflet::leafletProxy(mapId = map_id,
-    #                       session = parent_session) %>%
-    #   leaflet::addRasterImage(x = species_richness,
-    #                           layerId = "species_richness_raster",
-    #                           colors = species_richness_pal)
-
+        raster_domain <- terra::values(select_raster()) %>%
+          unique() %>%
+          sort()
+        raster_labels <- raster_domain
+        raster_pal <- leaflet::colorNumeric(
+          "magma",
+          raster_domain,
+          na.color = "transparent",
+          reverse = T
+        )
+        # Update Specific Sidebar
+        output$sidebarInfo <- renderUI({
+          tagList(
+            h1("Wildfire Vulnerability"),
+            util_ui_simple_legend_element(label = "Higher Vulnerability", colour = "#5B177E"),
+            util_ui_simple_legend_element(label = "Lower Vulnerability", colour = "#FEB078"),
+            p("This is a simple map to show how vulnerable different parts of Bowen Island are to biodiversity loss if a wildfire were to happen — not predicting fires, just showing where damage could be worst."),
+            p("We base this on three things: how steep the land is (fires spread faster uphill), which direction it faces (south-facing slopes tend to burn hotter), and what kind of habitat is there (some forests and sensitive ecosystems are more easily harmed by fire).")
+          )
+        })
+      }
+      #### Update Leaflet Map ####
+      leaflet::leafletProxy(mapId = map_id,
+                            session = parent_session) %>%
+        leaflet::clearControls() %>%
+        leaflet::clearImages() %>%
+        leaflet::addRasterImage(
+          x = select_raster(),
+          colors = raster_pal
+        ) %>%
+        leaflet::addLegend(
+          colors = raster_pal(raster_domain),
+          labels = raster_labels,
+          labFormat = labelFormat(),
+          title = raster_group
+        )
+    })
   })
 }
 
