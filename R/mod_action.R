610981e75e698dcf08b176708d776f260a2e4703
Jay Matsushiba <jay.matsushiba@gmail.com>
2025-07-26 12:06:22 +0900

    finished action view

--- R/mod_action.R
+++ R/mod_action.R
@@ -11,8 +11,20 @@ mod_action_ui <- function(id) {
   ns <- NS(id)
   tabPanel(
     "action_panel",
-    h1("Action"),
-    p("Under Construction")
+    bslib::card(
+      bslib::card_body(
+        tagList(
+          h1("Potential Protected Areas: Public Lands"),
+          util_ui_simple_legend_element(label = "Relatively Higher Value", colour = "#7a0177"),
+          util_ui_simple_legend_element(label = "Relatively Lower Value", colour = "#feebe2"),
+          util_ui_simple_legend_element(label = "Existing Protected Areas", colour = "#a1d76a"),
+          p("Protected areas on Bowen Island are crucial for safeguarding ecosystems and biodiversity from increasing threats such as development and habitat fragmentation."),
+          p("This map was created using Prioritizr software to identify the top 30% of public lands (Crown, municipal, and Metro Vancouver) with the highest conservation value. Areas were limited to those outside existing protected areas and weighted by their connectivity to those protected areas."),
+          em(strong("Note: "), "While this map can provide high-level view of biodiversity, this should not replace site-specific environmental assessments and consultation with experts.")
+        )
+      )
+    ),
+    docs_link
   )
 }
 
@@ -22,20 +34,54 @@ mod_action_ui <- function(id) {
 mod_action_server <- function(id, map_id, parent_session){
   moduleServer(id, function(input, output, session){
     ns <- session$ns
-#
-#     species_richness <- terra::rast(here::here("inst/extdata/bowen_sdm_richness.tif")) %>%
-#       terra::project("epsg:4326")
-#     species_richness_group <- "Species Richness"
-#     species_richness_domain <- c(0, terra::minmax(species_richness)[2])
-#     species_richness_pal <- leaflet::colorNumeric(c('#edf8fb','#b2e2e2','#66c2a4','#2ca25f','#006d2c'), species_richness_domain,
-#                                                   na.color = "transparent")
-#
-#     leaflet::leafletProxy(mapId = map_id,
-#                           session = parent_session) %>%
-#       leaflet::addRasterImage(x = species_richness,
-#                               layerId = "species_richness_raster",
-#                               colors = species_richness_pal)
 
+    #### Add Potential Protected Areas Raster ####
+    protected_areas_raster <- terra::rast(here::here("inst/extdata/7_action/potential_protected_areas.tif")) %>%
+      terra::project("epsg:4326")
+
+    raster_domain <- seq(from = 1, to = 5)
+    raster_labels <- c("Low", "Medium-Low", "Medium", "Medium-High", "High")
+    raster_colours <- c("#feebe2", "#fbb4b9", "#f768a1", "#c51b8a", "#7a0177")
+    raster_pal <- leaflet::colorFactor(
+      raster_colours,
+      raster_domain,
+      na.color = "transparent"
+    )
+
+    #### Add Existing Protected Areas Vector ####
+    bowen_pa <- here("inst/extdata/7_action/existing_protected_areas.gpkg") %>%
+      sf::st_read()
+
+    leaflet::leafletProxy(mapId = map_id,
+                          session = parent_session) %>%
+      leaflet::clearControls() %>%
+      leaflet::clearImages() %>%
+      leaflet::clearGroup(group = "clear_each_update") %>%
+      leaflet::addRasterImage(
+        x = protected_areas_raster,
+        layerId = "protected_areas_raster",
+        colors = raster_pal
+      ) %>%
+      leaflet::addPolygons(
+        data = bowen_pa,
+        group = "clear_each_update",
+        color = "#a1d76a",
+        stroke = FALSE,
+        fillOpacity = 1,
+        smoothFactor = 0.2
+      ) %>%
+      leaflet::addLegend(
+        colors = raster_pal(raster_domain),
+        layerId = "protected_areas_raster_legend",
+        labels = raster_labels,
+        title = "Poten. Protected Areas",
+      ) %>%
+      leaflet::addLegend(
+        colors = "#a1d76a",
+        layerId = "protected_areas_vector_legend",
+        labels = "Present",
+        title = "Exist. Protected Areas",
+      )
   })
 }
 
