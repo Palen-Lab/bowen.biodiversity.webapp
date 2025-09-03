0da49d22a8d96aa3edbf7e77d3bb0c81f1ac9564
Jay Matsushiba <jay.matsushiba@gmail.com>
2025-09-03 16:01:42 +0900

    overlay page finished!

--- R/mod_overlay.R
+++ R/mod_overlay.R
@@ -20,6 +20,7 @@ mod_overlay_ui <- function(id) {
             choices = c(
               "Choose Category",
               "Land Use",
+              "Human Disturbance",
               "Habitats - Freshwater",
               "Habitats - Terrestrial",
               "Threats - Development",
@@ -57,6 +58,7 @@ mod_overlay_server <- function(id, map_id, parent_session){
       terra::values() %>%
       sum(na.rm = T)
 
+    ## Land Use
     # Load Private Land - vector
     bowen_pm <- privateland %>%
       sf::st_transform(4326)
@@ -71,18 +73,31 @@ mod_overlay_server <- function(id, map_id, parent_session){
       sf::st_transform(4326) %>%
       sf::st_cast("MULTIPOLYGON") %>%
       sf::st_union()
+
+    ## Human Disturbance
+    bowen_hd <- terra::rast(here::here("inst/extdata/4_people/bowen_human_footprint_recl.tif"))
+    bowen_hd_p <- bowen_hd %>%
+      terra::project("epsg:3857", method = "near")
+
+    ## Habitats
     # Load Freshwater Habitats - raster
     bowen_fw <- terra::rast(here::here("inst/extdata/3_habitats/fw_richness.tif"))
     bowen_fw_p <- bowen_fw %>% terra::project("epsg:4326", method = "near") # Project raster to use with Leaflet
-
     # Load Terrestrial Habitats - raster
     # TODO: replace with terrestrial only richness raster
     bowen_te <- terra::rast(here::here("inst/extdata/3_habitats/total_habitat_richness.tif"))
     bowen_te_p <- bowen_te %>% terra::project("epsg:4326", method = "near") # Project raster to use with Leaflet
 
+    ## Threats - Development
     # Load Development Potential - vector
-    bowen_dp <- sf::st_read(here::here("inst/extdata/6_threats/development_potential.gpkg"))
+    bowen_dp <- sf::st_read(here::here("inst/extdata/6_threats/development_potential.gpkg")) %>%
+      sf::st_transform(4326)
+    # Development Potential - raster from vector
+    bowen_dp_r <- bowen_dp %>%
+      terra::vect() %>%
+      terra::rasterize(zonation, cover=T)
 
+    ## Threats - Wildfire
     # Load Wildfire - raster
     bowen_wf <- terra::rast(here::here("inst/extdata/6_threats/fire_index_40m.tif")) %>%
       terra::project("epsg:3857", method = "near")
@@ -165,7 +180,7 @@ mod_overlay_server <- function(id, map_id, parent_session){
             colors = zonation_pal
           )
       }
-      else if(input$selectGroup == "Land Use") {
+      else if (input$selectGroup == "Land Use") {
         # Disaggregate to get closer overlap area estimates later
         zonation_d <- zonation_og %>%
           terra::disagg(2)
@@ -242,31 +257,70 @@ mod_overlay_server <- function(id, map_id, parent_session){
             colors = zonation_pal
           )
       }
-      else if(input$selectGroup == "Habitats - Freshwater") {
+      else if (input$selectGroup == "Human Disturbance") {
+        # Apply grey out effect and update map
+        top_pct_zonation_vect(map, top_pct_zonation, top_pct)
+        # Overlap values
+        bowen_hd_pz <- bowen_hd %>% terra::project(zonation_og)
+        res <- values_in_out(zonation_og, top_pct_zonation, bowen_hd_pz)
+        inside_mean <- res$inside_vals %>% mean()
+        outside_mean <- res$outside_vals %>% mean()
+
+        inside_high_count <- res$inside_vals[res$inside_vals == 3] %>%
+          length()
+        inside_medium_count <- res$inside_vals[res$inside_vals == 2] %>%
+          length()
+        inside_low_count <- res$inside_vals[res$inside_vals == 1] %>%
+          length()
+        outside_high_count <- res$outside_vals[res$outside_vals == 3] %>%
+          length()
+        outside_medium_count <- res$outside_vals[res$outside_vals == 2] %>%
+          length()
+        outside_low_count <- res$outside_vals[res$outside_vals == 1] %>%
+          length()
+
+        # Update sidebar
+        sidebarTable <- data.frame(
+          type = c("Inside", "Outside"),
+          mean = c(round(inside_mean, 2), round(outside_mean, 2)),
+          high = c(inside_high_count, outside_high_count),
+          medium = c(inside_medium_count, outside_medium_count),
+          low = c(inside_low_count, outside_low_count)
+        )
+        output$specific_sidebarInfo <- renderUI({
+          "Human Disturbance"
+        })
+        output$sidebarTable <- DT::renderDT(
+          DT::datatable(sidebarTable[,c("type", "high", "medium", "low")], options = list(
+            paging = F,
+            searching = F,
+            ordering = F,
+            lengthChange = F,
+            info = F
+          ),
+          rownames = FALSE,
+          colnames = c("High (ha)", "Medium (ha)", "Low (ha)")
+          )
+        )
+      }
+      else if (input$selectGroup == "Habitats - Freshwater") {
         # Apply grey out effect and update map
         top_pct_zonation_vect(map, top_pct_zonation, top_pct)
 
+        # Overlap values
+        bowen_hd_pz <- bowen_hd %>% terra::project(zonation_og)
+        res <- values_in_out(zonation_og, top_pct_zonation, bowen_hd_pz)
+        inside_sum <- res$inside_vals %>% sum()
+        outside_sum <- res$outside_vals %>% sum()
+        total_sum <- inside_sum + outside_sum
+
         # Update Text and Table
-        # Overlap between top % and freshwater habitats
-        total_zonation_fw_count <- bowen_fw %>%
-          terra::project(top_pct_zonation) %>%
-          terra::not.na(falseNA=T) %>%
-          terra::values() %>%
-          sum(na.rm = T)
-
-        top_pct_zonation_fw_count <- bowen_fw %>%
-          terra::project(top_pct_zonation) %>%
-          terra::mask(top_pct_zonation) %>%
-          terra::not.na(falseNA=T) %>%
-          terra::values() %>%
-          sum(na.rm = T)
-
         sidebarTable <- data.frame(
           type = c("Inside", "Outside"),
-          ncells = c(top_pct_zonation_fw_count, (total_zonation_fw_count - top_pct_zonation_fw_count))
+          ncells = c(inside_sum, (total_sum - inside_sum))
         ) %>%
           dplyr::mutate(area_ha = ncells / 10) %>%
-          dplyr::mutate(pct = round(ncells / total_zonation_fw_count * 100))
+          dplyr::mutate(pct = round(ncells / total_sum * 100))
 
         output$specific_sidebarInfo <- renderUI({
           "Freshwater Habitat Richness"
@@ -289,36 +343,15 @@ mod_overlay_server <- function(id, map_id, parent_session){
         top_pct_zonation_vect(map, top_pct_zonation, top_pct)
 
         # Update Text and Table
-        # Overlap between top % and terrestrial habitats
-        total_zonation_te_mean <- bowen_te_p %>%
-          terra::project(top_pct_zonation) %>%
-          terra::values() %>%
-          mean(na.rm = T)
-
-        bowen_te_p_msk <- bowen_te_p %>%
-          terra::project(zonation_og) %>%
-          terra::mask(zonation_og)
-
-        # Mean richness inside
-        top_pct_zonation_te_mean_in <- bowen_te_p_msk %>%
-          terra::mask(top_pct_zonation) %>%
-          terra::values() %>%
-          mean(na.rm = T)
-        # Mean richness outside
-        top_pct_zonation_te_out_vals <- bowen_te_p_msk %>%
-          terra::mask(top_pct_zonation, inverse=T) %>%
-          terra::values()
-        # hacky, for some reason there is just a single cell with value when 100% is selected
-        # should not be any cells, can't figure out why there is one
-        # removes the first two unique values (which are NA and one cell)
-        top_pct_zonation_te_out_vals_u <- top_pct_zonation_te_out_vals %>%
-          unique()
-        top_pct_zonation_te_mean_out <- setdiff(top_pct_zonation_te_out_vals, top_pct_zonation_te_out_vals_u[1:2]) %>%
-          mean(na.rm = T)
+        # Overlap values
+        bowen_te_pz <- bowen_te_p %>% terra::project(zonation_og)
+        res <- values_in_out(zonation_og, top_pct_zonation, bowen_te_pz)
+        inside_mean <- res$inside_vals %>% mean()
+        outside_mean <- res$outside_vals %>% mean()
 
         sidebarTable <- data.frame(
           type = c("Inside", "Outside"),
-          mean = c(round(top_pct_zonation_te_mean_in, 2), round(top_pct_zonation_te_mean_out, 2))
+          mean = c(round(inside_mean, 2), round(outside_mean, 2))
         )
         output$specific_sidebarInfo <- renderUI({
           "Habitat Richness"
@@ -336,47 +369,53 @@ mod_overlay_server <- function(id, map_id, parent_session){
           )
         )
       }
-      else if(input$selectGroup == "Threats - Development") {
+      else if (input$selectGroup == "Threats - Development") {
         # Apply grey out effect and update map
         top_pct_zonation_vect(map, top_pct_zonation, top_pct)
+
+        # Overlap values
+        bowen_dp_pz <- bowen_dp_r %>% terra::project(zonation_og)
+        res <- values_in_out(zonation_og, top_pct_zonation, bowen_dp_pz)
+        inside_mean <- res$inside_vals %>% mean()
+        outside_mean <- res$outside_vals %>% mean()
+
+        # Update sidebar
+        sidebarTable <- data.frame(
+          type = c("Inside", "Outside"),
+          mean = c(round(inside_mean, 2), round(outside_mean, 2))
+        )
+        output$specific_sidebarInfo <- renderUI({
+          "Development Potential"
+        })
+        output$sidebarTable <- DT::renderDT(
+          DT::datatable(sidebarTable[,c("type", "mean")], options = list(
+            paging = F,
+            searching = F,
+            ordering = F,
+            lengthChange = F,
+            info = F
+          ),
+          rownames = FALSE,
+          colnames = c("Mean Potential Dev. Units / ha")
+          )
+        )
 
       }
       else if(input$selectGroup == "Threats - Wildfire") {
         # Apply grey out effect and update map
         top_pct_zonation_vect(map, top_pct_zonation, top_pct)
 
-        # Update Text and Table
-        # Overlap between top % and terrestrial habitats
-        # total_zonation_wf_mean <- bowen_wf %>%
-        #   terra::project(top_pct_zonation) %>%
-        #   terra::values() %>%
-        #   mean(na.rm = T)
-
-        bowen_wf_msk <- bowen_wf %>%
+        # Overlap values
+        bowen_wf_pz <- bowen_wf %>%
           terra::project(zonation_og) %>%
-          terra::resample(zonation_og, method = "average") %>%
-          terra::mask(zonation_og)
-
-        # Mean richness inside
-        top_pct_zonation_wf_mean_in <- bowen_wf_msk %>%
-          terra::mask(top_pct_zonation) %>%
-          terra::values() %>%
-          mean(na.rm = T)
-        # Mean richness outside
-        top_pct_zonation_wf_out_vals <- bowen_wf_msk %>%
-          terra::mask(top_pct_zonation, inverse=T) %>%
-          terra::values()
-        # hacky, for some reason there is just a single cell with value when 100% is selected
-        # should not be any cells, can't figure out why there is one
-        # removes the first two unique values (which are NA and one cell)
-        top_pct_zonation_wf_out_vals_u <- top_pct_zonation_wf_out_vals %>%
-          unique()
-        top_pct_zonation_wf_mean_out <- setdiff(top_pct_zonation_wf_out_vals, top_pct_zonation_wf_out_vals_u[1:2]) %>%
-          mean(na.rm = T)
+          terra::resample(zonation_og, method = "average")
+        res <- values_in_out(zonation_og, top_pct_zonation, bowen_wf_pz)
+        inside_mean <- res$inside_vals %>% mean()
+        outside_mean <- res$outside_vals %>% mean()
 
         sidebarTable <- data.frame(
           type = c("Inside", "Outside"),
-          mean = c(round(top_pct_zonation_wf_mean_in, 2), round(top_pct_zonation_wf_mean_out, 2))
+          mean = c(round(inside_mean, 2), round(outside_mean, 2))
         )
         output$specific_sidebarInfo <- renderUI({
           "Vulnerability Fire Index"
@@ -479,6 +518,27 @@ mod_overlay_server <- function(id, map_id, parent_session){
           #   weight = strokeWeight
           # )
       }
+      else if (input$selectGroup == "Human Disturbance") {
+        # Leaflet parameters
+        raster_domain <- c(1, 2, 3)
+        raster_labels <- c("1","2","3")
+        raster_pal <- leaflet::colorFactor(
+          c("#ffeda0", "#feb24c", "#f03b20"),
+          raster_domain,
+          na.color = "transparent"
+        )
+        # Update Leaflet
+        map %>%
+          leaflet::clearGroup(
+            "overlay_polygons"
+          ) %>%
+          leaflet::clearImages() %>%
+          leaflet::clearControls() %>%
+          leaflet::addRasterImage(
+            x = bowen_hd_p,
+            colors = raster_pal
+          )
+      }
       else if (input$selectGroup == "Habitats - Freshwater") {
         # Leaflet parameters
         raster_domain <- terra::values(bowen_fw_p) %>%
@@ -530,19 +590,11 @@ mod_overlay_server <- function(id, map_id, parent_session){
           )
       }
       else if (input$selectGroup == "Threats - Development") {
-        # Leaflet parameters
-        raster_domain <- terra::values(bowen_te_p) %>%
-          unique()
-        raster_labels <- raster_domain
-        raster_colours <- c('#ffffcc','#d9f0a3','#addd8e','#78c679','#41ab5d','#238443','#005a32')
-        raster_colour_func <- colorRampPalette(raster_colours) # For custom palette
-        raster_colour_ramp <- raster_colour_func(length(raster_domain)) %>% rev()
-
-        # raster_colour_ramp <- viridis::mako(5, end = 0.8, direction = -1)
-        raster_pal <- leaflet::colorNumeric(
-          raster_colour_ramp,
-          raster_domain,
-          na.color = "transparent"
+        # Update Leaflet Map Parameters
+        layer_domain <- c(0, bowen_dp$potential_units)
+        pal <- leaflet::colorNumeric(
+          palette = "YlOrRd",
+          domain = layer_domain
         )
         # Update Leaflet
         map %>%
@@ -551,9 +603,16 @@ mod_overlay_server <- function(id, map_id, parent_session){
           ) %>%
           leaflet::clearImages() %>%
           leaflet::clearControls() %>%
-          leaflet::addRasterImage(
-            x = bowen_te_p,
-            colors = raster_pal
+          leaflet::addPolygons(
+            data = bowen_dp,
+            # TODO: apply layerId iteratively, since there are multiple polygons
+            # layerId = "bowen_dp",
+            group = "overlay_polygons",
+            color = ~pal(potential_units),
+            fillColor = ~pal(potential_units),
+            opacity = 1,
+            fillOpacity = 1,
+            weight = 3
           )
       }
       else if (input$selectGroup == "Threats - Wildfire") {
@@ -590,8 +649,9 @@ mod_overlay_server <- function(id, map_id, parent_session){
 ## To be copied in the server
 # mod_overlay_server("values_1")
 
-## top_pct_zonation_vect
-# For providing the greyed out effect on areas not in top %
+#' top_pct_zonation_vect
+#' @description
+#' For providing the greyed out effect on areas not in top %
 top_pct_zonation_vect <- function(map, data, top_pct) {
   # Create polygon that is inverse of selected raster cells
   top_pct_zonation_vect <- terra::ifel(is.na(data), 1, NA) %>%
@@ -616,5 +676,43 @@ top_pct_zonation_vect <- function(map, data, top_pct) {
   # TODO: fix that polygon doesn't line up with raster exactly on leaflet map
 }
 
-
+#' Overlap
+#' @param area_mask SpatRaster, has values for study area and NA for
+#' outside study area
+#' @param selected_mask SpatRaster, has values for selected pixels
+#' within study area, and NA for not selected pixels and outside study
+#' area
+#' @param input_rast SpatRaster, has values that will be extracted for
+#' inside and outside study area.
+#' @returns named list, for values inside and outside selected pixels
+#' from input_rast. Removes NA values.
+values_in_out <- function(area_mask, selected_mask, input_rast) {
+  # Check if inputs have same CRS
+  a <- terra::crs(area_mask)
+  b <- terra::crs(selected_mask)
+  c <- terra::crs(input_rast)
+  if(!all(sapply(list(a,b,c), function(x) x == c))) {
+    stop("Inputs do not have the same CRS.")
+  }
+  # Create raster masked to pixels within area_mask
+  input_masked <- terra::mask(input_rast, area_mask)
+  # Create raster masked to pixels within selected_mask
+  ins_rast <- terra::mask(input_masked, selected_mask)
+  # Create raster masked to pixels outside selected_mask
+  out_rast <- terra::mask(input_masked, selected_mask, inverse = T)
+  # Values inside selected pixels
+  ins_vals <- terra::values(ins_rast)
+  ins_vals_nn <- ins_vals[!is.na(ins_vals)]
+  # Values outside selected pixels
+  out_vals <- terra::values(out_rast)
+  out_vals_nn <- out_vals[!is.na(out_vals)]
+  # hacky, for some reason there is just a single cell with value when 100% is selected
+  # should not be any cells, can't figure out why there is one
+  # removes the first two unique values (which are NA and one cell)
+  if(length(out_vals_nn) <= 1) {
+    out_vals_nn <- NULL
+  }
+  # Return list of lists
+  list("inside_vals" = ins_vals_nn, "outside_vals" = out_vals_nn)
+}
 
