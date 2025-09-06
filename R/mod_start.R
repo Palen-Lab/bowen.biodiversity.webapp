6dbe19148b1c7bbe056b7981f8f3c459e0b17517
Jay Matsushiba <jay.matsushiba@gmail.com>
2025-09-06 12:48:55 +0900

    content on first page

--- R/mod_start.R
+++ R/mod_start.R
@@ -14,10 +14,12 @@ mod_start_ui <- function(id) {
       tagList(
         h1("Welcome!"),
         p("The Bowen Island Biodiversity Data Atlas serves as the interactive information center for supporting decision-making around sustainable development on Bowen Island."),
+        h3("Inputs"),
         p("Knowledge and data was gathered from a wide range of sources, including academia, government, NGO, and local community members, to be integrated into this Bowen Island Biodiversity Data Atlas."),
-        p("The existing information are used as inputs to generate Conservation Values across Bowen Island."),
-        p("This Conservation Values is then overlaid with potential threats, identifying key locations for further attention."),
-        p("Finally, based on the Conservation Values, we explore Action that could mitigate risk towards biodiversity."),
+        h3("Analysis"),
+        p("Values showcases the Conservation Values output, which takes all of our inputs and algorithmically identifies the highest conservation value locations. In Threats, we conduct additional analyses to generate spatial layers that map potential threats to biodiversity across Bowen Island."),
+        h3("Outputs"),
+        p("These Conservation Values are used to create outputs, identifying key locations for further attention. Protected Areas shows potential future areas of protection. Overlay allows interactive exploration to compare the Conservation Values layer to other important layers."),
         actionButton(
           inputId = "start_page_button",
           label = "START",
